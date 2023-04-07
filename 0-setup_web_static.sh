58f6b79772d50d79c1b70f7ca4edd4289e594e22
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2023-04-07 05:18:05 +0000

    done

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Script that configures Nginx server with some folders and files
+apt-get -y update
+apt-get -y install nginx
+service nginx start
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+# Create a symbolic link
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Give ownership of the /data/ folder to the ubuntu user and group
+chown -R ubuntu:ubuntu /data/
+# Update Nginx configuration
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
+exit 0
